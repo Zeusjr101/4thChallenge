f016cde920cede52bb21922b60841c9cbe714f65
JesusUjr101 <JesusUjr101@gmail.com>
2023-09-16 12:17:35 -0400

    made changes to HTML,CSS, Javacript

---
+++ assets/Js/javascript.js
@@ -0,0 +1,47 @@
+
+let questOne = document.querySelectorAll('.questOne');
+let questTwo = document.querySelectorAll('.questTwo');
+let questthee = document.querySelectorAll('.questthee');
+let questfour = document.querySelectorAll('.questfour');
+let questfive = document.querySelectorAll('.questfive');
+
+function startTest() {
+    let quizTest = confirm("Lets play a quiz Game!")
+    if (quizTest === true) {
+        location.reload();
+    } else {
+        alert("Why are you leaving this page?")
+    }
+}
+
+let questions = [ {
+    questOne: "What is the capital of France?",
+    answer: [
+        { Text: "Paris" },
+        { Text: "Toulouse" },
+        { Text: "Strasbourg" },
+        { Text: "Nantes" }
+        
+    ]
+}];
+
+let questionElement = document.querySelector('.question');
+let buttoncontainer = document.querySelector('.answerBtn');
+
+let questionIndex = 0;
+
+function nextQuestion() {
+    if (questionIndex < questions.length) {
+        questionElement.textContent = questions[questionIndex].questOne;
+        buttoncontainer.innerHTML = "";
+        for (let i = 0; i < questions[questionIndex].answer.length; i++) {
+            let button = document.createElement('button');
+            button.textContent = questions[questionIndex].answer[i].Text;
+            button.setAttribute('class', 'btn btn-primary');
+            buttoncontainer.appendChild(button);
+        }
+        questionIndex++;
+    } else {
+        location.reload();
+    }
+}
