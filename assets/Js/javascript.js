116e230916c998129502b573ea3abfcdec5a88bb
JesusUjr101 <JesusUjr101@gmail.com>
2023-09-18 23:19:26 -0400

    update and changed alot of code update REadme

--- assets/Js/javascript.js
+++ assets/Js/javascript.js
@@ -1,77 +1,132 @@
+// when we lock the start button a timer starts counting=done
+// once we answer a quesiton we are present with other questions
+// when answer incorreclty  time is subtracted from timer
+// all question are answered or the timer reaches 0
+// game is over when all questions are answered or the timer reaches 0
+// i can save intitials and my score 
+// need to add a score counter
 
-let questOne = document.querySelectorAll('.questOne');
-let questTwo = document.querySelectorAll('.questTwo');
-let questthee = document.querySelectorAll('.questthee');
-let questfour = document.querySelectorAll('.questfour');
-let questfive = document.querySelectorAll('.questfive');
+var timerElement = document.querySelector("#timer");
+var startQuizButton = document.querySelector("#start-quiz");
 
-function startTest() {
-    let quizTest = confirm("Lets play a quiz Game!")
-    if (quizTest === true) {
-        location.reload();
+
+function startQuiz() {
+  var timeLeft = 20; 
+
+  startQuizButton.disabled = true; 
+  timerInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timerElement.textContent = timeLeft; 
     } else {
-        alert("Why are you leaving this page?")
+      clearInterval(timerInterval); 
+      timerElement.textContent = "Time's up!";
+      startQuizButton.disabled = false;
+      
     }
+  }, 1000); // Update the timer every 1 second
 }
 
-let questions = [ {
-    questOne: "What is the capital of France?",
-    answer: [
-        { Text: "Paris" },
-        { Text: "Toulouse" },
-        { Text: "Strasbourg" },
-        { Text: "Nantes" }
-        
-    ]
-}];
 
-let questionElement = document.querySelector('.question');
-let buttoncontainer = document.querySelector('.answerBtn');
-
-let questionIndex = 0;
-
-function nextQuestion() {
-    if (questionIndex < questions.length) {
-        questionElement.textContent = questions[questionIndex].questOne;
-        buttoncontainer.innerHTML = "";
-        for (let i = 0; i < questions[questionIndex].answer.length; i++) {
-            let button = document.createElement('button');
-            button.textContent = questions[questionIndex].answer[i].Text;
-            button.setAttribute('class', 'btn btn-primary');
-            buttoncontainer.appendChild(button);
-        }
-        questionIndex++;
-    } else {
-        location.reload();
+document.addEventListener("DOMContentLoaded", function () {
+  let questions = [
+    {
+      question: "What is HTML?",
+      answers: ["Hypertext Markup Language", "Hyperlink and Text Markup Language", "Home Tool Markup Language", "Hyperlink Text Makeup Language"],
+      correctAnswer: "Hypertext Markup Language",
+    },
+    {
+      question: "What is CSS?",
+      answers: ["Cascading Style Sheet", "Computer Style Sheet", "Creative Style System", "Colorful Style Sheet"],
+      correctAnswer: "Cascading Style Sheet",
+    },
+    {
+      question: "Which of these options does NOT require the use of parentheses?",
+      answers: ["A. Console.log", "B. Prompt", "C. Alert ", "D.innerHTML"],
+      correctAnswer: "D. innerHTML",
+    },
+    {
+      question: "What is JavaScript?",
+      answers: ["A programming language", "A markup language", "A database language", "A styling language"],
+      correctAnswer: "A programming language",
+    },
+    {
+      question: "JavaScript uses what kind of interface to access the DOM?",
+      answers: ["A. CSS", "B. An API", "C.HTML", "D. Nothing else"],
+      correctAnswer: "B. An API",
+    },
+    {
+      question: "Which of these is not valid(Hit pay attention to if the method should return on thin, or many things?",
+      answers: ["A. document.getElementsBYTagName(tagName)", "B. document.getElementsByClassName(className)", "C. document.getElementsByid(idName)", "D. styling language"],
+      correctAnswer: "C. document.getElementsByid(idName) ",
+    },
+    {
+      question: "Which of the following is not a valid method rfor generating output to the screen?",
+      answers: ["A. doument.wite", "B. Print", "C. Alert", "D. Prompt"],
+      correctAnswer: "B. Print",
+    },
+  ];
+    let currentQuestionIndex = 0;
+    let scores = 0;
+  
+    let questionElement = document.getElementById("question");
+    let answerButtons = document.querySelectorAll(".btn");
+    let nextButton = document.getElementById("next-btn");
+    let scoresElement = document.getElementById("scores");
+  
+    function startGame() {
+      currentQuestionIndex = 0;
+      points = 0;
+      nextButton.style.display = "none";
+      setNextQuestion();
     }
-}
-var startButton = document.querySelector("#start");
-var timerEL = document.querySelector("#timer");
-
-var startButton = document.querySelector("#start");
-var timerEL = document.querySelector("#timer");
-
-letbtn = document,getEkenebtbyid('btn');
-var myTimer;
-var timerRunning = false;
-
-BigInt.addEventListener("click",function(){
-    if (timerRunning) {
-    clearInterval(timerEL);
-    timerRunning = false;
-    BigInt.textContent = "start"; 
-    }else {
-        timerRunning = true;
-        BigInt.textContent = "stop";
-        CSSLayerBlockRule();
+  
+    function setNextQuestion() {
+      resetState();
+      showQuestion(questions[currentQuestionIndex]);
     }
-});
+  
+    function showQuestion(question) {
+      questionElement.innerText = question.question;
+      question.answers.forEach((answer, index) => {
+        answerButtons[index].innerText = answer;
+        answerButtons[index].addEventListener("click", selectAnswer);
+      });
+    }
+  
+    function resetState() {
+      answerButtons.forEach((button) => {
+        button.classList.remove("correct");
+        button.classList.remove("incorrect");
+        button.removeEventListener("click", selectAnswer);
+      });
+    }
+  
+    function selectAnswer(event) {
+      let selectedButton = event.target;
+      let correct = questions[currentQuestionIndex].correctAnswer === selectedButton.innerText;
+  
+      if (correct) {
+        selectedButton.classList.add("correct");
+        scores++;
+        scoresElement.innerText = scores;
+      } else {
+        selectedButton.classList.add("incorrect");
 
-function clock() {
-    var c = 0;
-    function myClock() {
-        timerEL.textContent = Timer; " + c"
-        c++;
-}
-    myTimer = setInterval(myClock, 1000);
-}
+        // You can implement a penalty or deduct points here if needed.
+      }
+  
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        nextButton.style.display = "block";
+      } else {
+        // The quiz is over
+        // You can implement what happens when the quiz is finished here.
+      }
+    }
+  
+    nextButton.addEventListener("click", setNextQuestion);
+  
+    // Start the game
+    startGame();
+  });
